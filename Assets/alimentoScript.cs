d6aac6ee2748b239e8409848f8467a8016b23cc5
FedericoGaubeca <>
2024-06-19 15:41:12 -0300

    Initial commit

---
+++ Assets/alimentoScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alimentoScript : MonoBehaviour
+{
+    public int valorAlimenticio;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/playerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public int nivelHambre;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        alimentoScript alimento;
+        alimento = col.GetComponent<alimentoScript>();
+
+        if (alimento)
+        {
+            TomarAlimento(alimento.valorAlimenticio);
+            Destroy(col.gameObject);
+        }
+        Debug.Log(col.gameObject);
+    }
+
+    void TomarAlimento(int valorAlimenticio)
+    {
+        nivelHambre -= valorAlimenticio;
+        if (nivelHambre <= 0)
+        {
+            nivelHambre = 0;
+        }
+    }
+}
